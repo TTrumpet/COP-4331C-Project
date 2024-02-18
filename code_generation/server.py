8dda2ac0bb04debc79e43729ea74c75a72d39cc5
TTrumpet <tran.tinau@gmail.com>
2024-02-18 17:49:45 -0500

    Added unit test for code_generation and changed to faster algorithm

--- code_generation/server.py
+++ code_generation/server.py
@@ -1,4 +1,5 @@
 from datasets import load_dataset
+import random
 from flask import Flask, request, flash
 from flask_cors import CORS, cross_origin
 from flask_restful import Resource, Api
@@ -10,19 +11,20 @@ app = Flask(__name__)
 api = Api(app)
 CORS(app, supports_credentials=True)
 
-class CodeGeneration():
+class CodeGeneration(Resource):
     def __init__(self, language):
         self.language = language
-        data = load_dataset("Fsoft-AIC/the-vault-function", split_set=["train"], languages=[language], streaming=True)
+        data = load_dataset("Fsoft-AIC/the-vault-function", split_set=["train"], languages=[language], streaming=True, trust_remote_code=True)
         self.data = data['train']
         data['train'] = data['train'].shuffle()
 
+    @app.route('/start_game/<language>', methods=['GET'])
     def code_gen(self):
-        self.data = self.data.shuffle()
-        for sample in self.data:
-            return sample['original_string']
+        #self.data = self.data.shuffle()
+        for index, sample in enumerate(self.data):
+            if not random.randint(0, index):
+                return sample['original_string']
 
 #@app.route('/code_gen', methods=['GET'])
 
-app.add_resource(CodeGeneration, '/start_game/<language>')
-app.get_code('/start_game/<language>', 'code_gen', CodeGeneration.code_gen())
+api.add_resource(CodeGeneration,'/start_game/<language>')
