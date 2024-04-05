7f193e2aeae2e4fe3e7ff7780edec1397e0f0be9
Axel Lopez <lilturtle3334@gmail.com>
2024-04-05 17:14:37 -0400

    stats half complete

--- code_generation/server.py
+++ code_generation/server.py
@@ -34,7 +34,7 @@ app = Flask(__name__)
 api = Api(app)
 
 #Set Uri to equal "mysql+pymysql://"User":"pass"@127.0.0.1:3306/"dbName"
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:password@127.0.0.1:3306/mydb'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:root@127.0.0.1:3306/mydb'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False #can be true if pref
 
 #for connection with ang
@@ -60,6 +60,10 @@ class UserSettings(db.Model): #changes to db are manditory for this to work
     time = db.Column(db.Integer, default=15)
     textcolor = db.Column(db.String(9), nullable=False, default='FFFFFF')
     textsize = db.Column(db.Integer, default=25)
+    chartyped = db.Column(db.Integer, default=0)
+    totaltime = db.Column(db.Integer, default=0)
+    charsincorrect = db.Column(db.Integer, default=0)
+    totalscore = db.Column(db.Integer, default=0)
 
     #returns a string representation of object
     def __repr__(self):
@@ -138,7 +142,11 @@ def populate_profile():
             "language": user.language,
             "time": user.time,
             "textcolor": user.textcolor,
-            "textsize": user.textsize
+            "textsize": user.textsize,
+            "chartyped" : user.chartyped,
+            "totaltime" : user.totaltime,
+            "charsincorrect" : user.charsincorrect,
+            "totalscore" : user.totalscore
         }
         return jsonify(response_data), 200
     else:
@@ -163,6 +171,10 @@ def update_profile():
     user.textcolor = data.get('textcolor', user.textcolor)
     user.time = data.get('time', user.time)
     user.textsize = data.get('textsize', user.textsize)
+    user.chartyped = data.get('chartyped', user.chartyped)
+    user.totaltime = data.get('totaltime', user.totaltime)
+    user.charsincorrect = data.get('charsincorrect', user.charsincorrect)
+    user.totalscore = data.get('totalscore', user.totalscore)
 
     try:
         db.session.commit()
--- user-interface/src/app/stats/stats.component.ts
+++ user-interface/src/app/stats/stats.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { ProfileService } from '../profile.service';
 @Component({
   selector: 'app-stats',
   standalone: true,
@@ -9,4 +9,33 @@ import { Component } from '@angular/core';
 })
 export class StatsComponent {
 
+  charsTotal: number = 1;
+  charsIncorrect: number = 0;
+  totaltime: number = 1;
+
+
+  totalScore: number = 1;
+  avgCPM : number = 0;
+  accuracy: number = 0;
+
+
+  constructor(private profileService: ProfileService){}
+  ngOnInit(){
+    this.setStats();
+  }
+
+  setStats(){
+    this.charsTotal = this.profileService.chartyped;
+    this.charsIncorrect = this.profileService.charsincorrect;
+    this.totaltime = this.profileService.totaltime;
+    this.totalScore = this.profileService.totalscore;
+
+    if(this.charsTotal != 0)
+      this.avgCPM = Math.floor(this.charsTotal/(this.totaltime/60));
+    if(this.charsTotal != 0)
+      this.accuracy = Math.floor( (1- (this.charsIncorrect/this.charsTotal)) *100 );
+
+    
+
+  }
 }
