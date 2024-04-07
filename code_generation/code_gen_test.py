47881d3f8f0ce1fc81884752944afaac996d19e7
TTrumpet <tran.tinau@gmail.com>
2024-04-07 16:07:36 -0400

    adding text files

--- code_generation/code_gen_test.py
+++ code_generation/code_gen_test.py
@@ -6,12 +6,20 @@ if __name__ == '__main__':
         data = load_dataset("Fsoft-AIC/the-vault-function", split_set=["test"], trust_remote_code=True, cache_dir='./cache')
         data = data['test']
         data = data.shuffle()
-        #f = open(language+'.txt', 'w')
 
-        lang = input("input language: ")
+        for language in ['Java', 'JavaScript', 'Python', 'Ruby', 'Rust', 'Golang', 'C#', 'C++', 'C', 'PHP']:
+            f = open('./text_files/' + language +'.txt', 'w') 
+            counter = 10
+            for index, sample in enumerate(data):
+                if counter == 0:
+                    break
+                if sample['language'] == language:
+                    #print(sample['code'].split('\n'))
+                    try:
+                        f.write(sample['code'])
+                        f.write('\r\n')
+                    except:
+                        counter += 1
+                    counter -= 1
 
-        for index, sample in enumerate(data):
-            if sample['language'] == lang:
-                #print(sample['code'].split('\n'))
-                print(sample['code'])
-                exit()
+                
